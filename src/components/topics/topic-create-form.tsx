8653943600fecaeb7cc2935d93d2e6f260f43b59
Norbert Faragó <n.farago@grundfos.com>
2025-01-03 18:14:32 +0100

    Added topic create form

---
+++ src/components/topics/topic-create-form.tsx
@@ -0,0 +1,12 @@
+import {
+    Input,
+    Button,
+    Textarea,
+    Popover,
+    PopoverTrigger,
+    PopoverContent
+} from '@nextui-org/react';
+
+export default function TopicCreateForm() {
+    return <div>TopicCreateForm</div>;
+}
