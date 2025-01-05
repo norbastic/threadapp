66ad9b63a13d1d3b5e1626b3c7d9b8a71ae4a8b7
norbastic <norbert.faragooo@gmail.com>
2025-01-05 17:27:51 +0100

    Create topic form is complete

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,5 +1,27 @@
 "use server";
 
+import type { Topic } from "@prisma/client";
+import { redirect } from "next/navigation";
+import { revalidatePath } from "next/cache";
+import { z } from "zod";
+import * as authAll from "@/auth";
+import { auth } from "@/auth";
+import { db } from "@/db";
+import paths from "@/paths";
+
+const createTopicSchema = z.object({
+    name: z.string().min(3).regex(/^[a-z-]+$/, { message: "Must be lowercase letters or dashes." }),
+    description: z.string().min(10),
+});
+
+interface CreateTopicFormState {
+    errors: {
+        name?: string[];
+        description?: string[];
+        _form?: string[];
+    }
+}
+
 export async function createComment() {
     const { createComment : _createComment } = await import("./create-comment");
     return _createComment;
@@ -10,18 +32,60 @@ export async function createPost() {
     return _createPost;
 }
 
-export async function createTopic() {
-    const { createTopic : _createTopic } = await import("./create-topic");
-    return _createTopic;
+export async function createTopic(formState: CreateTopicFormState, formData: FormData) : Promise<CreateTopicFormState> {
+    //revalidate the homepage
+    const name = formData.get("name");
+    const description = formData.get("description");
+
+    const result = createTopicSchema.safeParse({
+        name,
+        description
+    });
+
+    const session = await auth();
+
+    if (!session || !session.user) {
+        return {
+            errors: {
+                _form: ["You must be signed in to do this."]
+            }
+        };
+    }
+
+    let topic: Topic;
+    try {
+        topic = await db.topic.create({
+            data: {
+                slug: result.data!.name,
+                description: result.data!.description
+            }
+        })
+
+    } catch(err: unknown) {
+        if (err instanceof Error) {
+            return {
+                errors: {
+                    _form: [err.message]
+                }
+            }
+        } else {
+            return {
+                errors: {
+                    _form: ["Something went wrong"]
+                }
+            }
+        }
+
+    }
+
+    revalidatePath("/");
+    redirect(paths.topicShow(topic.slug));
 }
 
-import * as auth from "@/auth";
-
 export async function signIn() {
-    return await auth.signIn("github");
+    return await authAll.signIn("github");
 }
 
 export async function signOut() {
-    return await auth.signOut();
+    return await authAll.signOut();
 }
-
--- src/components/topics/topic-create-form.tsx
+++ src/components/topics/topic-create-form.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import {
     Input,
     Button,
@@ -6,7 +8,55 @@ import {
     PopoverTrigger,
     PopoverContent
 } from '@nextui-org/react';
+import * as actions from "@/actions";
+import { useActionState, startTransition } from "react";
 
 export default function TopicCreateForm() {
-    return <div>TopicCreateForm</div>;
+    const [formState, action] = useActionState(actions.createTopic, {
+        errors: {}
+    });
+
+    function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+        event.preventDefault();
+        const formData = new FormData(event.currentTarget);
+        startTransition(() => {
+            action(formData);
+        });
+    }
+
+    return (
+        <Popover placement="left">
+            <PopoverTrigger>
+                <Button color="primary">Create a Topic</Button>
+            </PopoverTrigger>
+            <PopoverContent>
+                <form action={action}>
+                    <div className="flex flex-col gap-4 p-4 w-80">
+                        <h3 className="text-lg">Create a Topic</h3>
+                        <Input name="name"
+                               label="Name"
+                               labelPlacement="outside"
+                               placeholder="Name"
+                               isInvalid={!!formState.errors.name}
+                               errorMessage={formState.errors.name?.join(", ")}
+                        />
+                        <Textarea name="description"
+                                  label="Description"
+                                  labelPlacement="outside"
+                                  placeholder="Describe your topic"
+                                  isInvalid={!!formState.errors.description}
+                                  errorMessage={formState.errors.description?.join(", ")}
+                        />
+                        {
+                            formState.errors._form ?
+                                <div className="rounded p-2 bg-red-400 border border-red-400">
+                                    { formState.errors._form.join(", ")}
+                                </div> : null
+                        }
+                        <Button type="submit">Submit</Button>
+                    </div>
+                </form>
+            </PopoverContent>
+        </Popover>
+    );
 }
